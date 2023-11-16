cdb774957b2dd1e788767b97f0f88d4de8890844
oumhanane <79924229+oumhanane@users.noreply.github.com>
2023-11-16 23:16:37 +0100

    ajout fichier data.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import { useState } from "react"
+// import React, { useState, useEffect } from 'react';
+import {useState} from 'react';
+import {useEffect} from 'react';
+
 
 function App() {
+  const [data, setData] = useState(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(new FormData(e.target))
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/data.json');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
   }
 
-  console.log('render')
+  // Utilisez les données dans votre application, par exemple :
+  console.log(data.users);
+  console.log(data.products);
 
-  return <form onSubmit={handleSubmit}>
-    <input type="text" name="firstname" defaultValue="adresse"/>
-    <button>Envoyer</button>
-  </form>
+  return (
+    <div>
+      {/* Votre application React */}
+      <h1>Bonjour les gens</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
