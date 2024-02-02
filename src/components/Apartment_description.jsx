2977b1836b0efb9ed74feda0865fb3018427d9b7
oumhanane <79924229+oumhanane@users.noreply.github.com>
2024-02-02 21:03:40 +0100

    modif apartment_description.jsx apartment_description.css

--- src/components/Apartment_description.jsx
+++ src/components/Apartment_description.jsx
@@ -9,19 +9,34 @@ function Apartment_description(props) {
     setIsContentVisible(!isContentVisible);
   };
 
+  console.log('isContentVisible:', isContentVisible);
+
+  
+  const contentStyle = {
+    backgroundColor: 'rgb(240, 240, 225)',
+    padding: '1rem',
+    fontSize: '1.2rem',
+    color: '#FF6060',
+    overflow: 'hidden',
+    maxHeight: isContentVisible ? '1000px' : '0',
+    transition: 'max-height 0.3s ease-in-out',
+  };
+
   // const contentClass = (isContentVisible ? "visible" : "hidden") + "description__content";
   // const chevronClass = (isContentVisible ? "fa-chevron-up" : "fa-chevron-down") + " fas";
 
-  const contentClass = `description_content ${isContentVisible ? "visible" : "hidden"}`;
-  const chevronClass = `fas ${isContentVisible ? "fa-chevron-up" : "fa-chevron-down"}`;
+  // const contentClass = `description_content ${isContentVisible ? "visible" : "hidden"}`;
+  // const chevronClass = `fas ${isContentVisible ? "fa-chevron-up" : "fa-chevron-down"}`;
 
   return (
     <div className="description">
       <p className="description_header" onClick={showContent}>
         <span>{props.title}</span>
-        <i className={chevronClass}></i>
+        {/* <i className={chevronClass}></i> */}
+        <i className={`fas ${isContentVisible ? "fa-chevron-up" : "fa-chevron-down"}`}></i>
       </p>
-      <p className={contentClass}>{props.content}</p>
+      {/* <p className={contentClass}>{props.content}</p> */}
+      <p style={contentStyle}>{props.content}</p>
     </div>
   )
 }
